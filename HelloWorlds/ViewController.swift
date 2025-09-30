7583ab7e660b8c0142f57fdbd5786011cdc4dda4
Sever2686 <sever_3@live.ru>
2025-09-30 19:06:04 +0300

    Initial Commit

---
+++ HelloWorlds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorlds
+//
+//  Created by Алексей Шевченко on 30.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
